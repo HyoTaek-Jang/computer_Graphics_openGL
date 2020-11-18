53c95dcdf41ac34b1ddf821680e2a1b92ed0968b
HyoTaekJang <hyotaek9812@gmail.com>
2020-11-19 03:37:14 +0900

    orbit camera success

--- OpenGL/ConsoleApplication1/ConsoleApplication1/OpenGLTest.cpp
+++ OpenGL/ConsoleApplication1/ConsoleApplication1/OpenGLTest.cpp
@@ -16,12 +16,18 @@
 
 void render(GLFWwindow* window);
 void init();
+void mouseButtonCallback(GLFWwindow*, int, int, int);
+void cursorMotionCallback(GLFWwindow*, double, double);
 
 int main()
 {
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(800, 800, "201721070", 0, 0);
     glfwMakeContextCurrent(window);
+
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetCursorPosCallback(window, cursorMotionCallback);
+
     glewInit();
 
     init();
@@ -41,6 +47,12 @@ GLuint vertexArray = 0;
 GLuint vao = 0;
 GLuint elementArray = 0;
 int count = 0;
+float cameraDistance = 3;
+glm::vec3 sceneCenter = glm::vec3(0, 0, 0);
+float cameraYaw = 0.f;
+float cameraPitch = 0.f;
+int lastX=0, lastY=0;
+float cameraFov = 60.f;//필드오브뷰 카메라가 보는 각도, 크면 많이보이공ㅇ
 
 Program program;
 
@@ -79,12 +91,61 @@ void render(GLFWwindow* window) {
     glUseProgram(program.programID);
     glBindVertexArray(vertexArray);
 
+    mat4 projMat = perspective(cameraFov * 3.141592f / 180, w / float(h), 0.01f, 100.f);
+
+    vec3 cameraPosition = vec3(0,0,cameraDistance);
+    cameraPosition = vec3(rotate(cameraPitch, vec3(-1, 0, 0)) * vec4(cameraPosition, 1));
+    cameraPosition = vec3(rotate(cameraYaw, vec3(0, 1, 0)) * vec4(cameraPosition, 1));
+    mat4 viewMat = lookAt(cameraPosition, sceneCenter, vec3(0, 1, 0));
+
+
     rotAngle += 0.1 / 180.f * 3.141592;
     GLuint loc = glGetUniformLocation(program.programID, "modelMat");
+
     mat4 rotMat = rotate(rotAngle, glm::vec3(0, 1, 0));
-    glUniformMatrix4fv(loc, 1, 0, value_ptr(rotMat));
+   // glUniformMatrix4fv(loc, 1, 0, value_ptr(rotMat));
+
+    glUniformMatrix4fv(loc, 1, 0, value_ptr(rotate(90/180.f*3.141592f,vec3(1,0,0))));
+   
+    loc = glGetUniformLocation(program.programID, "viewMat");
+    glUniformMatrix4fv(loc, 1, 0, value_ptr(viewMat));
+
+    loc = glGetUniformLocation(program.programID, "projMat");
+    glUniformMatrix4fv(loc, 1, 0, value_ptr(projMat));
+
+    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementArray);
     glDrawElements(GL_TRIANGLES, nTriangles[0] * 3, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
 
 }
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mod) {
+    if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS) {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastX = int(xpos);
+        lastY = int(ypos);
+
+        printf("%d %d \n", lastX, lastX);
+    }
+}
+
+
+
+ void cursorMotionCallback(GLFWwindow* window, double xpos, double ypos) {
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
+        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+            cameraFov += (ypos - lastY) / 300;
+        }
+        else {
+        cameraPitch+= (ypos - lastY) / 300; //300픽셀 움직이면 1라디안
+        cameraPitch = glm::clamp(cameraPitch, -2.f, 2.f); // 제한 걸어두는거
+        cameraYaw -= (xpos - lastX) / 300;
+        lastX = int(xpos);
+        lastY = int(ypos);
+
+        }
+        }
+    }
+
--- OpenGL/ConsoleApplication1/ConsoleApplication1/shader.vert
+++ OpenGL/ConsoleApplication1/ConsoleApplication1/shader.vert
@@ -3,15 +3,11 @@
 #version 410 core
 
 layout(location=0) in vec3 in_Position;
-layout(location=1) in vec2 in_TexCoord;
 uniform mat4 viewMat = mat4(1,0,0,0,0,1,0,0,0,0,1,0,0,0,-3,1);
 uniform mat4 projMat = mat4(1.299038, 0, 0, 0, 0, 1.732051, 0, 0, 0, 0, -1.002002, -1.0, 0, 0, -0.2002, 0);
-uniform mat4 modelMat;
-uniform mat3 textMat;
-out vec2 textCoord;
+uniform mat4 modelMat = mat4(1);
 
 void main(void) {
           vec4 worldPos = modelMat*vec4(in_Position, 1.0);
           gl_Position= projMat*viewMat*worldPos;
-          textCoord = (textMat*vec3(in_TexCoord,1)).xy;
 }
